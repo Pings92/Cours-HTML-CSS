2583a7fff86e53fc30676905f81cbaf1e35c52c0
pierickj <pierick.jacquet@gmail>
2026-02-10 11:49:12 +0100

    Exo Ternaires Jour 7 midi break

--- link/exodivers.js
+++ link/exodivers.js
@@ -140,8 +140,8 @@ let maListe = [0,4,7,8]; //C'est une instance
 const resultNumber = new maCalculatrice(maListe) ;
 resultNumber.afficher();
 
-let testt = [6,2,9,2,0,2] //C'est une instance
-const resultNumber2 = new maCalculatrice(testt) ;
+let test = [6,2,9,2,0,2] //C'est une instance
+const resultNumber2 = new maCalculatrice(test) ;
 resultNumber2.afficher();
 
 //---------------- EXERCICE 5
@@ -195,6 +195,8 @@ class spell{
             console.log(`Attaque: ${this.strenght}`);
             console.log(`Titre: ${this.titre}`);
         }
+        showCaracterSecondMethod(){
+            console.log(`Nom: ${this.nom}\nSanté: ${this.health}\nAttaque: ${this.strenght}\nTitre: ${this.titre}`)      }
         }
 
     class thief extends personnage{
@@ -202,6 +204,10 @@ class spell{
             super (nom, health, strenght, titre);
             this.bonusStat = bonusStat;
         }            
+        showCaracterStat(){
+            super.showCaracter();
+            console.log (`Caractéristique Bonus: ${this.bonusStat}`)
+      }
     }
 
     class monk extends personnage{
@@ -219,7 +225,11 @@ class spell{
         constructor(nom, health, strenght, titre, bonusStat){
             super (nom, health, strenght, titre);
             this.bonusStat = bonusStat;
-        }            
+        }
+        showCaracterStat(){
+            super.showCaracter();
+            console.log (`Caractéristique Bonus: ${this.bonusStat}`)
+      }            
     }
 
     class shovelKnight extends personnage{
@@ -227,7 +237,97 @@ class spell{
             super (nom, health, strenght, titre);
             this.bonusStat = bonusStat;
         }            
+        showCaracterStat(){
+            super.showCaracterSecondMethod();
+            console.log (`Caractéristique Bonus: ${this.bonusStat}`)
+      }
     }            
-    
-    let personnagePrincipal = new monk("Jean", "1000", "120", "moine", "High PV / High Def")
+
+    let personnagePrincipal = new thief("Djidane", "1000", "120", "Voleur", "High luck")
     personnagePrincipal.showCaracterStat();
+
+    let personnageSecondaire = new shovelKnight("Sean", "1000", "120", "Shovel Knight", "Stunt ennemy")
+    personnageSecondaire.showCaracterStat();    
+
+    let personnageTerciaire = new redMage ("Elliot", "1000", "120", "Mage Rouge", "Master Dark & White Magic")
+    personnageTerciaire.showCaracterStat();  
+
+    let personnageQuarter = new monk("Aerith", "1000", "120", "Moine", "High PV / High Def")
+    personnageQuarter.showCaracterStat();
+
+    console.table(personnageTerciaire)
+
+    console.log({
+        nom: personnagePrincipal.nom,
+        class: personnagePrincipal.titre,
+    })
+
+    // EXERCICE TERNAIRE
+ // Exo 1 => bon
+    let age = 25
+    let acces = age >= 18 ? "Autorisé" : "Refusé"
+
+    // Exo 2 Bon
+    prenom = prompt("Quelle est votre prénom?");
+    let verif = prenom =! "" ? prenom : "Aucun prénom saisi";
+    console.log (verif);
+
+    // Exo 3 Bon
+    nbr = prompt ("Rentrez un nombre: ");
+    pairImpair = nbr%2 == 0? "Pair": "Impair"
+    console.log (pairImpair)
+
+    // Exo 4 Bon
+    nbrUser = prompt("Donnez une note entre 0 et 20 : ")
+    grade = nbrUser >=16 ? "Excellent" : nbrUser >= 12 ? "Bien" : nbrUser >= 10 ? "Moyen" : "insuffisant";
+    console.log(grade); 
+
+    // Exo 5
+    tableau = [-6, 45, 32, -72, 0, -23, 39];
+    const positifNegatif = tableau.map((x) => x > 0 ? "+" : x < 0 ? "-" : "0" );
+    console.log(positifNegatif);
+
+    // ------------- Debut pas bon
+    // for (i = 0; i <= tableau.length; i++){
+    //     positifNegatif = tableau[i] > 0 ? tableau.map("+") :tableau[i] < 0 ? tableau.map("-") : tableau.map(0);
+    // }
+    // console.log(positifNegatif);
+    // positifNegatif = tableau.map > 0 ? tableau.map("+") :tableau[i] < 0 ? tableau.map("-") : tableau.map(0);
+    // console.log(positifNegatif);
+
+    // const array = [1, 4, 9, 16];
+    // const mapped = tableau.map((x) => x > 0 ? "+" : x < 0 ? "-" : "0" ) : tableau[i] < 0 ? tableau.map((x) => "-") : tableau.map ((x) => 0); 
+    // console.log(mapped);
+
+    // for (const element of tableau) {
+    //     array.forEach(tableau.map()=> {
+    //         console.log(element)
+    //     });
+    //     tableau.map> 0 ? tableau.map((x) => "+") : tableau[i] < 0 ? tableau.map((x) => "-") : tableau.map ((x) => 0); 
+    // }
+    // -------- FIN PAS BON
+
+// -------------------- EXERCICE TERNAIRE => Filtrer  EXERCICE 6 BON
+ 
+    tableau = [-6, 30, 45, 32, -72, 0, -23, 39];
+    tableauPair = tableau.filter((x) =>  x%2 == false);
+    console.log(tableauPair);
+
+    // Tentative pas bonne
+    // let tableauFiltrer=[];
+    // for (i = 0; i <= tableau.length; i++){
+    //     tableauFiltrer = tableau[i]%2 == 0 ? tableauFiltrer.push(tableau[i]) : "";
+    //     console.log(tableauFiltrer);
+    // }
+    // tableauFiltrer = tableau.filter((x) => tableau[x]%2 == 0 ? "pair" : "impair" );
+    // console.log(tableauFiltrer);
+
+    // fin tentative pas bonne
+
+// ----------- Exercice 7 BON
+
+    tableau = ["papepipopu", "babe", "caceci", "dad", "crabe", "tatetito", "melon", "fa", "gageg", "lalelilolu"];    
+    motLong = tableau.filter((motDuTableau) => motDuTableau.length == 6 );
+    console.log(motLong);
+
+// -------------- EXERCICE 8 
